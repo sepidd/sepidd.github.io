df7d624ee9bb1cedc075849cf0528f663733bc82
sepiddsh <sepideh@gatech.edu>
2020-10-11 12:21:30 -0400

    electronics

---
+++ routes/home.js
@@ -0,0 +1,20 @@
+const featuredCompanies = require('../lib/featured-companies')
+const featuredApps = require('../lib/featured-apps')
+
+module.exports = (req, res) => {
+  const apps = []
+  Object.keys(featuredApps).forEach((k) => {
+    // One app out of each category
+    const category = featuredApps[k]
+    const random = Math.floor(Math.random() * category.length)
+
+    apps.push(category[random])
+  })
+
+  const context = Object.assign(req.context, {
+    companies: featuredCompanies,
+    apps,
+  })
+
+  res.render('home', context)
+}
